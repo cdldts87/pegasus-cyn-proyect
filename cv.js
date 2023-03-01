8c7dfbdc441032bfcbba24e94c521cd1a11a7322
cdldts87 <126626416+cdldts87@users.noreply.github.com>
2023-03-01 15:40:49 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,9 @@
+<form action="" method="post">
+        <label>Nombre</label>
+        <input type="text" name="nombre" placeholder="Ingrese su nombre" />
+        <label>Apellido</label>
+        <input type="text" name="apellido" placeholder="Ingrese su apellido" />
+        <label>E-mail</label>
+        <input type="email" name="email" placeholder="Ingrese su E-mail" />
+        <input type="submit" value="Enviar"/>
+    </form>
